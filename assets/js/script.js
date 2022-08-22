48670ae6e6c508c4376e24876fa59a5e149c8ef5
coderyesh <yeshwanthchauhan2000@gmail.com>
2022-08-22 22:31:47 +0530

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,92 @@
+'use strict';
+
+/**
+ * adding event on element
+ */
+const addEventOnElement = function (element, type, listener) {
+  if (element.length > 1) {
+    for (let i=0; i<element.length; i++) {
+      element[i].addEventListener(type,listener);
+    }
+  }else {
+    element.addEventListener(type, listener);
+  }
+}
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navLinks =document.querySelectorAll("[data-nav-link]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNav = function () {
+  navbar.classList.toggle("active");
+  this.classList.toggle("active");
+}
+
+addEventOnElement(navToggler, "click", toggleNav);
+
+const closeNav = function() {
+  navbar.classList.remove("active");
+  navToggler.classList.remove("active");
+}
+addEventOnElement(navLinks, "click", closeNav);
+
+
+/**adding active class on header & backtotop */
+
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 50) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
+/**Hero tab button */
+const tabBtns = document.querySelectorAll("[data-tab-btn]");
+
+let lastClickedTabBtn = tabBtns[0];
+
+const changeTab = function () {
+  lastClickedTabBtn.classList.remove("active");
+  this.classList.add("active");
+  lastClickedTabBtn = this;
+}
+
+addEventOnElement(tabBtns, "click", changeTab);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
